38e96cd8733423c867c6fcf1cbae7c848d262b0e
Treechcer <treechcer@gmail.com>
2025-10-30 20:43:22 +0100

    started on compiling it into obj files, it works, not really full script for this, but it does for now most of the needed stuff

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -15,7 +16,6 @@ size_t hashString(std::string toHash) {
 }
 
 std::vector <std::string> getFilesToCopy(std::string path = "."){
-    std::string path = path;
     std::vector<std::string> toCopy;
     for (const auto &entry : std::filesystem::directory_iterator(path)) { // thanks stack overflow!!
         //std::cout << entry.path() << std::endl;
@@ -30,7 +30,12 @@ std::vector <std::string> getFilesToCopy(std::string path = "."){
         else if (std::regex_search(file, std::regex(".h$"))) {
             toCopy.push_back(file);
         }
+        else if (entry.is_directory()) {
+            toCopy = getFilesToCopy(file);
+        }
     }
+
+    return toCopy;
 }
 
 int main() {
@@ -43,9 +48,37 @@ int main() {
     std::filesystem::create_directories("./.temp"); // for creating the build folder (called .temp for now)
     
 	for (int i = 0; i < toCopy.size(); i++) {
-        std::ifstream src(toCopy[i], std::ios::binary);
-        std::ofstream dst(std::filesystem::path(".temp") / toCopy[i], std::ios::binary);
-        dst << src.rdbuf();
+        std::filesystem::create_directories(std::filesystem::path(".temp") / std::filesystem::path(toCopy[i]).parent_path());
+
+        std::ifstream ifs(toCopy[i]);
+        std::string contentPNew((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+        //std::cout << hashString(contentPNew) << std::endl;
+
+        int hashPNew = hashString(contentPNew);
+
+        std::ifstream ifs2(std::filesystem::path(".temp") / toCopy[i].substr(2));
+        std::string contentPold((std::istreambuf_iterator<char>(ifs2)), (std::istreambuf_iterator<char>()));
+        //std::cout << hashString(contentPold) << std::endl;
+
+        int hashPOld = hashString(contentPold);
+
+        if (hashPNew != hashPOld){
+            std::ifstream src(toCopy[i], std::ios::binary);
+            std::filesystem::path path = std::filesystem::path(".temp") / toCopy[i].substr(2);
+            std::ofstream dst(path, std::ios::binary);
+            dst << src.rdbuf();
+
+            std::string cmd = "g++ -c ";
+            cmd.append(path.string());
+            cmd.append(" -o ");
+            cmd.append(std::filesystem::path(".temp/").string());
+            cmd.append(toCopy[i].substr(2, toCopy[i].size() - 5));
+            cmd.append("o");
+
+            std::cout << cmd << std::endl;
+
+            system(cmd.c_str());
+        }
     }
 
     return 0;
