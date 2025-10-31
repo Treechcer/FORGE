b92975fdc2f59a3100cd424daa7399a92acbff19
Treechcer <treechcer@gmail.com>
2025-10-31 17:44:05 +0100

    it doesn't work... but when I copy the commands it makes it works? why?

--- main.cpp
+++ main.cpp
@@ -39,50 +39,91 @@ std::vector <std::string> getFilesToCopy(std::string path = "."){
     return toCopy;
 }
 
+void buildProject(std::vector <std::string> objFile, std::string name="FORGE.exe"){
+    std::string cmdObjpart = "";
+    for (int i = 0; i < objFile.size(); i++){
+        cmdObjpart.append(objFile[i]);
+        cmdObjpart.append(" ");
+    }
+    
+    //std::cout << cmdObjpart;
+
+    std::string cmd = "g++ ";
+    cmd.append(cmdObjpart);
+    cmd.append("-o ");
+    cmd.append(name);
+    //cmd.append(" -mconsole");
+
+    for (int i = 0; i < cmd.size(); i++){
+        if (cmd[i] == '\\'){
+            cmd[i] = '/';
+        }
+    }
+    
+    std::cout << cmd << std::endl;
+    system(cmd.c_str());
+}
+
 int main() {
     std::vector<std::string> toCopy = getFilesToCopy();
 
-    for (int i = 0; i < toCopy.size(); i++) { // debug / temp writing out files
-        std::cout << toCopy[i] << std::endl;
-    }
+    //for (int i = 0; i < toCopy.size(); i++) { // debug / temp writing out files
+    //    std::cout << toCopy[i] << std::endl;
+    //}
 
     std::filesystem::create_directories("./.temp"); // for creating the build folder (called .temp for now)
 
     size_t hashEmpty = hashString("");
+
+    std::vector<std::string> objFiles;
 
     for (int i = 0; i < toCopy.size(); i++) {
         std::filesystem::create_directories(std::filesystem::path(".temp") / std::filesystem::path(toCopy[i]).parent_path());
 
         std::ifstream ifs(toCopy[i]);
         std::string contentPNew((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
-        std::cout << hashString(contentPNew) << std::endl;
+        //std::cout << hashString(contentPNew) << std::endl;
 
         size_t hashPNew = hashString(contentPNew);
 
         std::ifstream ifs2(std::filesystem::path(".temp") / toCopy[i].substr(2));
         std::string contentPold((std::istreambuf_iterator<char>(ifs2)), (std::istreambuf_iterator<char>()));
-        std::cout << hashString(contentPold) << std::endl;
+        //std::cout << hashString(contentPold) << std::endl;
 
         size_t hashPOld = hashString(contentPold);
+        std::string file = std::filesystem::path(".temp\\").string() + toCopy[i].substr(2, toCopy[i].size() - 5) + "o";
+        objFiles.push_back(file); //
 
         if (hashPNew != hashPOld && hashPNew != hashEmpty){
             std::ifstream src(toCopy[i], std::ios::binary);
             std::filesystem::path path = std::filesystem::path(".temp") / toCopy[i].substr(2);
             std::ofstream dst(path, std::ios::binary);
             dst << src.rdbuf();
-
+            
             std::string cmd = "g++ -c ";
             cmd.append(path.string());
             cmd.append(" -o ");
-            cmd.append(std::filesystem::path(".temp\\").string());
-            cmd.append(toCopy[i].substr(2, toCopy[i].size() - 5));
-            cmd.append("o");
+            cmd.append(std::filesystem::path (file).string());
+
+            //cmd.append(std::filesystem::path(".temp\\").string());
+            //cmd.append(toCopy[i].substr(2, toCopy[i].size() - 5));
+            //cmd.append("o");
 
             std::cout << cmd << std::endl;
+
+            //std::cout << file << std::endl;
+
+            //for (int i = 0; i < cmd.size(); i++) {
+            //    if (cmd[i] == '\\') {
+            //        cmd[i] = '/';
+            //    }
+            //}
 
             system(cmd.c_str());
         }
     }
+
+    buildProject(objFiles);
 
     return 0;
 }
