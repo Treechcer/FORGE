ac2e526b28ffe1c0361ae66a6d72face756b49e7
Treechcer <treechcer@gmail.com>
2025-11-01 19:32:12 +0100

    now the files are also copied, all paths are saved in a way too

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 
+std::filesystem::path FORGEPATH = ".\\.FORGE";
+
 size_t hashString(std::string toHash) {
     std::string text = toHash;
     std::hash<std::string> hasher;
@@ -26,19 +28,54 @@ std::vector<std::filesystem::path> getFiles(std::filesystem::path dir, std::vect
             paths.push_back(entry.path());
         }
         else if (entry.is_directory()){
-            getFiles(entry.path(), paths);
+            paths = getFiles(entry.path(), paths);
         }
     }
 
     return paths;
 }
 
+std::vector<std::filesystem::path> changePaths(std::vector<std::filesystem::path> paths) {
+    std::cout << "----------------------------------------\n";
+    for (int i = 0; i < paths.size(); i++){
+        std::string temp = paths[i].string();
+        temp.erase(0, 2);
+        paths[i] = std::filesystem::path(FORGEPATH / temp);
+        std::cout << temp << '\n';
+        std::cout << paths[i] << '\n';
+    }
+
+    return paths;
+}
+
+int copyFiles(std::vector<std::filesystem::path> pathBefore, std::vector<std::filesystem::path> pathAfter) {
+    if (pathAfter.size() != pathBefore.size()){
+        return 1;
+    }
+
+    for (int i = 0; i < pathAfter.size(); i++){
+        std::filesystem::create_directories(pathAfter[i].parent_path());
+        std::filesystem::copy(pathBefore[i], pathAfter[i], std::filesystem::copy_options::overwrite_existing);
+    }
+
+    return 0;
+}
+
 int main() {
     std::vector<std::filesystem::path> paths;
+    std::vector<std::filesystem::path> changedPaths;
     std::filesystem::path thisDir = ".";
-    std::filesystem::create_directory(".\\.FORGE");
+    std::filesystem::create_directory(FORGEPATH);
 
-    getFiles(thisDir, paths);
+    paths = getFiles(thisDir, paths);
+
+    changedPaths = changePaths(paths);
+
+    int status = copyFiles(paths, changedPaths);
+
+    if (status == 1){
+        return 0;
+    }
 
     return 0;
 }
