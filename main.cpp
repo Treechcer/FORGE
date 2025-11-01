932000d075037ee22902bf987fd48e2bcec7ce88
Treechcer <treechcer@gmail.com>
2025-11-01 19:02:17 +0100

    files are now as paths...

--- main.cpp
+++ main.cpp
@@ -14,7 +14,31 @@ size_t hashString(std::string toHash) {
     return hashValue;
 }
 
+std::vector<std::filesystem::path> getFiles(std::filesystem::path dir, std::vector<std::filesystem::path> paths) {
+    if (std::regex_match(dir.string(), std::regex(".*\\.git")) || std::regex_match(dir.string(), std::regex(".*\\.FORGE"))) {
+        return paths;
+    }
+    for (const auto &entry : std::filesystem::directory_iterator(dir)) {
+        std::string str = entry.path().string();
+        //std::cout << str << std::endl;
+        if (std::regex_match(str, std::regex(".*\\.cpp$")) || std::regex_match(str, std::regex(".*\\.h$"))) {
+            std::cout << entry.path() << "\n";
+            paths.push_back(entry.path());
+        }
+        else if (entry.is_directory()){
+            getFiles(entry.path(), paths);
+        }
+    }
+
+    return paths;
+}
+
 int main() {
+    std::vector<std::filesystem::path> paths;
+    std::filesystem::path thisDir = ".";
+    std::filesystem::create_directory(".\\.FORGE");
+
+    getFiles(thisDir, paths);
 
     return 0;
 }
