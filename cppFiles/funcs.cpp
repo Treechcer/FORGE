d3a739081288aa297438245953e1fb6820ce6f00
Treechcer <treechcer@gmail.com>
2025-11-16 13:27:00 +0100

    updated so path for Mac OS works - or should at least

--- cppFiles/funcs.cpp
+++ cppFiles/funcs.cpp
@@ -7,17 +7,24 @@
 #endif
 
 std::filesystem::path getExecFolder() {
-#if defined(_WIN32)
+#ifdef _WIN32
     char path[MAX_PATH];
     HMODULE hModule = GetModuleHandle(nullptr);
     if (hModule && GetModuleFileNameA(hModule, path, MAX_PATH)) {
         return std::filesystem::path(path);
     }
-#elif defined(__linux__)
+#elifdef __linux__
     char path[PATH_MAX];
     ssize_t count = readlink("/proc/self/exe", path, PATH_MAX);
     if (count != -1) {
         return std::filesystem::path(std::string(path, count));
+    }
+#elifdef __APPLE__
+    char path[PATH_MAX];
+    uint32_t size = sizeof(path);
+
+    if (_NSGetExecutablePath(path, &size) == 0) {
+        return std::filesystem::canonical(path);
     }
 #endif
     return {};
