d74924e3cd86a6d9d274d20374ea70feace825b0
Treechcer <treechcer@gmail.com>
2025-11-30 11:11:13 +0100

    now it compiles oh god why don't I programme on Linux code for linux?

--- cppFiles/buildApp.cpp
+++ cppFiles/buildApp.cpp
@@ -1,6 +1,6 @@
 #include "../headers/constants.h"
 #include "../headers/funcs.h"
-#include "../headers/inputFuncs.h"
+#include "../headers/configParser.h"
 #include <fstream>
 #include <iostream>
 #include <regex>
@@ -9,19 +9,19 @@
 void desktopFileCreate(bool terminal, bool instaEnd){
     std::cout << "TEST OUTPUT\n";
 
-#ifdef __linux__
+//#ifdef __linux__
     std::string desktopFile = "";
     std::string name = cfgVals("exeName");
     std::filesystem::path exec = getExecFolder();
     std::string term = (terminal) ? "true" : "false";
     std::string icon = (std::filesystem::absolute(LINUXRESOURCES) / "icon.png").lexically_normal().string();
     std::string preExec = std::regex_replace(std::filesystem::absolute((std::filesystem::path(name)).replace_extension("")).string(), std::regex(" "), "\\ ");
-    std::string exec = (instaEnd) ? preExec : "bash -c \"" + preExec + "; read -p 'Press Enter to exit...'\"";
+    std::string execC = (instaEnd) ? preExec : "bash -c \"" + preExec + "; read -p 'Press Enter to exit...'\"";
 
     desktopFile += "[Desktop Entry]\n";
     desktopFile += "Type=Application\n";
     desktopFile += "Name= " + name + "\n";
-    desktopFile += "Exec=" + exec + "\n";
+    desktopFile += "Exec=" + execC + "\n";
     desktopFile += "Terminal=" + term + "\n";
     desktopFile += "Icon=" + icon + "\n";
 
@@ -38,7 +38,7 @@ void desktopFileCreate(bool terminal, bool instaEnd){
     ofs1 << desktopFile;
     ofs1.close();
     std::cout << home / ".local" / "share" / "applications";
-#elif defined (__APPLE__)
-    std::cout << "comming soon..."
-#endif
+//#elif defined (__APPLE__)
+    std::cout << "comming soon...";
+//#endif
 }
